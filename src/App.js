bec09d130582b468c272c034b831d78015761575
Monica Barboza <barboza.monica2@gmail.com>
2019-06-27 16:51:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import db from './firebase'
 
-const firebase = require('firebase');
-// Required for side-effects
-require('firebase/firestore');
 
-require('./secrets');
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  // apiKey: process.env.APIKEY,
-  // authDomain: process.env.AUTHDOMAIN,
-  projectId: 'trekk-fdf31',
-});
-
-// console.log(${process.env.AUTHDOMAIN})
-
-var db = firebase.firestore();
 
 // db.collection('Users')
 //   .get()
@@ -30,13 +16,26 @@ var db = firebase.firestore();
 
 async function users() {
   let usersAll= await db.collection('Users').get();
-  console.log(usersAll.docs)
-  console.log(typeof usersAll.docs)
+  let users = await db.collection("Users").doc('oXLPsIdgUTNcph63Xeew').collection('friends').get();
+  users.forEach((friend) => {
+    console.log(friend.data())
+  })
+  console.log(users)
+
+
+
+  usersAll.forEach( async doc=>{
+    console.log(doc.id)
+    console.log(await db.collection("Users").doc('oXLPsIdgUTNcph63Xeew').collection('friends').get().docs)
+    //console.log(doc.get(doc.id).collection("friends").data())
+  });
+  // console.log(usersAll.docs)
+  // console.log(typeof usersAll.docs)
   // console.log(usersAll.docs.get('trips'))
-  db.collection('Users').get().then((user)=>{
-    user.docs.forEach(doc=>{
-      console.log(doc.data().name)
-      console.log(doc.get('friends'))
+  db.collection('Users').get().then((users)=>{
+    users.forEach(doc=>{
+      // console.log(doc.data())
+      // console.log(doc.get('friends'))
     })
   });
   // console.log('current Data', doc.data);
