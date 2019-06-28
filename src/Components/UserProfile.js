fb09c2d12522da44f59924d27d8990376b72a64b
Patricia de Boer <pdeboer05@gmail.com>
2019-06-28 10:14:19 -0500

    working Promiseall query

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -27,7 +27,9 @@ export class UserProfile extends Component {
     super();
     this.state = {
       trips: [],
+      info: [],
     };
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
   async componentDidMount() {
     // let users = await db
@@ -36,31 +38,57 @@ export class UserProfile extends Component {
     //   .get();
 
     // this.setState({ trips: Object.keys(users.data().trips) });
+    console.log('beginning of component did mount')
     let users = await db
       .collection('Users')
       .doc('lQNWEtdOGjXlIdmUIRb9')
       .get();
-    console.log(Object.values(users.data().Trips));
+    console.log(
+      'Object values user.data.trips:',
+      Object.values(users.data().Trips)
+    );
     let allTrips = Object.keys(users.data().Trips);
-    console.log(allTrips);
+    console.log('allTrips', allTrips);
     let info = [];
-    let trip = await db
-      .collection('Trips')
-      .doc('5ujEWhzj0Tgc4nCu5Jgq')
-      .get();
+    // let trip = await db
+    //   .collection('Trips')
+    //   .doc('5ujEWhzj0Tgc4nCu5Jgq')
+    //   .get();
 
-    // let details = allTrips.forEach(async trip => {
+    // allTrips.forEach(async trip => {
     //   let detailTrip = await db
     //     .collection('Trips')
     //     .doc(trip)
     //     .get();
-    //   return info.push(detailTrip);
+    //   const tripData = detailTrip.data();
+    //   info.push(tripData);
+    //   // this.setState({
+    //   //   info: this.state.info.push(detailTrip.data()),
+    //   // });
+    //   console.log('detailTrip.data: ', detailTrip.data());
     // });
-
-    console.log('trip: ', trip.data());
+    const mapReturn = allTrips.map(async trip => {
+      let detailTrip = await db
+        .collection('Trips')
+        .doc(trip)
+        .get();
+      const tripData = detailTrip.data();
+      info.push(tripData);
+      // this.setState({
+      //   info: this.state.info.push(detailTrip.data()),
+      // });
+      console.log('detailTrip.data: ', detailTrip.data());
+    });
+    await Promise.all(mapReturn)
+    console.log('just info array:', info);
+    this.setState({
+      info,
+    });
+    console.log('this.state.info in component did mount:', this.state.info[0]);
   }
 
   render() {
+    console.log('this.state.info in render:', this.state.info[1]);
     return (
       <div>
         <ListGroup>
