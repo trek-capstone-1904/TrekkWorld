f53cff755c9cf3dcec89bfa42608b27f8c5a663c
Brandon Sotiroff <bsotiroff1@gmail.com>
2019-06-29 17:21:13 -0500

    seed file

--- src/Components/TripSearch.js
+++ src/Components/TripSearch.js
@@ -1,64 +1,99 @@
-import React, { Component } from 'react';
-import { ListGroup } from 'react-bootstrap';
-import Trips from './TripList';
+import React, { useState } from 'react';
+import { ListGroup, InputGroup } from 'react-bootstrap';
 import db from '../firebase';
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 
-export class UserProfile extends Component {
-  constructor() {
-    super();
-    this.state = {
-      trips: [],
-    };
-  }
-  async componentDidMount() {
-    let users = await db
-      .collection('Users')
-      .doc('oXLPsIdgUTNcph63Xeew')
-      .get();
-    // console.log(users.data().trips);
-    this.setState({ trips: Object.keys(users.data().trips) });
-    // let details = Object.values(users.data().trips)
-    // console.log(tripss);
 
-    // users.forEach(trip => {
-    //   console.log(trip.data());
-    // });
-  }
+export const  TripSearch = () => {
+  
+  const [value, setValue] = useState('');
 
-  render() {
-    return (
-      <div>
-        <ListGroup>
-          {this.state.trips.map(trip => (
-            <Trips key={trip} trip={trip} />
-          ))}
-        </ListGroup>
-      </div>
-    );
-  }
+
+  var searchTerm = "Lima"
+  var tripsRef = db.collection("places");
+
+  tripsRef.where("locations", "==", searchTerm) 
+  
+  return (
+    <div>
+      <InputGroup className="mb-3">
+  <FormControl
+    placeholder="Recipient's username"
+    aria-label="Recipient's username"
+    aria-describedby="basic-addon2"
+  />
+  <InputGroup.Append>
+    <Button variant="outline-secondary" value={`${value}`}>Search</Button>
+  </InputGroup.Append>
+  </InputGroup>
+      <ListGroup>
+        {this.state.trips.map(trip => (
+          <Trips key={trip} trip={trip} />
+        ))}
+      </ListGroup>
+    </div>
+  );
+  
 }
+
+var tripLocationSearch = db.collectionGroup('trip_places')
+.where('location_id', '===', searchTerm);
+tripLocationSearch.get().then(function (querySnapshot) {
+ // Do something with these reviews!
+ console.log(querySnapshot)
+})
+
+const [values, loading, error] = useCollectionDataOnce(db.collection('Trips'), options)
+
+const [value, loading, error] = useDocument(
+db.doc('Users/lQNWEtdOGjXlIdmUIRb9'),
+{
+  valueListenOptions: { includeMetadataChanges: true },
+}
+);
+
 
 export default UserProfile;
 
-import React, { useState, useEffect } from 'react';
+export const UserProfileTrips = () => {
+ 
 
-function FriendStatus(props) {
-  const [isOnline, setIsOnline] = useState(null);
+  return (
+    <div>
+      {error && <strong>Error: {error}</strong>}
+      {loading && <span>Document: Loading...</span>}
+      {value &&
+        Object.keys(value.data().Trips).map(trip => (
+          <TripListItem key={trip} tripId={trip} />
+        ))}
+    </div>
+  );
+};
 
-  function handleStatusChange(status) {
-    setIsOnline(status.isOnline);
-  }
+export default UserProfileTrips;
 
-  useEffect(() => {
-    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+// import React, { useState, useEffect } from 'react';
 
-    return () => {
-      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
-    };
-  });
+// function FriendStatus(props) {
+//   const [isOnline, setIsOnline] = useState(null);
 
-  if (isOnline === null) {
-    return 'Loading...';
-  }
-  return isOnline ? 'Online' : 'Offline';
-}
+//   function handleStatusChange(status) {
+//     setIsOnline(status.isOnline);
+//   }
+
+//   useEffect(() => {
+//     ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+
+//     return () => {
+//       ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+//     };
+//   });
+
+//   if (isOnline === null) {
+//     return 'Loading...';
+//   }
+//   return isOnline ? 'Online' : 'Offline';
+// }
+
+
+
