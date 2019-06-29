5222791a25cc966d7569c3f721dd1e0429ffc63f
Brandon Sotiroff <bsotiroff1@gmail.com>
2019-06-29 18:03:42 -0500

    working on trip-search

--- src/Components/TripSearch.js
+++ src/Components/TripSearch.js
@@ -1,99 +1,42 @@
 import React, { useState } from 'react';
-import { ListGroup, InputGroup } from 'react-bootstrap';
+import { ListGroup, InputGroup, Button, FormControl } from 'react-bootstrap';
 import db from '../firebase';
-import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
+import { useCollectionOnce } from 'react-firebase-hooks/firestore';
 
+export const TripSearch = () => {
+  const [values, setValues] = useState({
+    searchTerm: '',
+  });
 
-export const  TripSearch = () => {
-  
-  const [value, setValue] = useState('');
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
+  const [snapshot, loading, error] = useCollectionOnce(
+    db.collection('Trips').where('locations', 'array-contains', 'Lima'),
+    options
+  );
+  console.log('snapshot');
+  // let searchTerm = 'Lima';
+  // let tripsRef = db.collection('Trips');
 
-  var searchTerm = "Lima"
-  var tripsRef = db.collection("places");
+  // tripsRef.where('locations', 'array-contains', searchTerm);
 
-  tripsRef.where("locations", "==", searchTerm) 
-  
   return (
     <div>
       <InputGroup className="mb-3">
-  <FormControl
-    placeholder="Recipient's username"
-    aria-label="Recipient's username"
-    aria-describedby="basic-addon2"
-  />
-  <InputGroup.Append>
-    <Button variant="outline-secondary" value={`${value}`}>Search</Button>
-  </InputGroup.Append>
-  </InputGroup>
-      <ListGroup>
-        {this.state.trips.map(trip => (
-          <Trips key={trip} trip={trip} />
-        ))}
-      </ListGroup>
-    </div>
-  );
-  
-}
-
-var tripLocationSearch = db.collectionGroup('trip_places')
-.where('location_id', '===', searchTerm);
-tripLocationSearch.get().then(function (querySnapshot) {
- // Do something with these reviews!
- console.log(querySnapshot)
-})
-
-const [values, loading, error] = useCollectionDataOnce(db.collection('Trips'), options)
-
-const [value, loading, error] = useDocument(
-db.doc('Users/lQNWEtdOGjXlIdmUIRb9'),
-{
-  valueListenOptions: { includeMetadataChanges: true },
-}
-);
-
-
-export default UserProfile;
-
-export const UserProfileTrips = () => {
- 
-
-  return (
-    <div>
-      {error && <strong>Error: {error}</strong>}
-      {loading && <span>Document: Loading...</span>}
-      {value &&
-        Object.keys(value.data().Trips).map(trip => (
-          <TripListItem key={trip} tripId={trip} />
-        ))}
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          value={values.searchTerm}
+          onChange={handleChange('searchTerm')}
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Search</Button>
+        </InputGroup.Append>
+      </InputGroup>
+      <ListGroup />
     </div>
   );
 };
-
-export default UserProfileTrips;
-
-// import React, { useState, useEffect } from 'react';
-
-// function FriendStatus(props) {
-//   const [isOnline, setIsOnline] = useState(null);
-
-//   function handleStatusChange(status) {
-//     setIsOnline(status.isOnline);
-//   }
-
-//   useEffect(() => {
-//     ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
-
-//     return () => {
-//       ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
-//     };
-//   });
-
-//   if (isOnline === null) {
-//     return 'Loading...';
-//   }
-//   return isOnline ? 'Online' : 'Offline';
-// }
-
-
-
