256cdfc4d4bd1af42e3bd7a8167eb08d936b97fc
Patricia de Boer <pdeboer05@gmail.com>
2019-07-01 00:30:34 -0500

    some basic clean up

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -4,7 +4,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 export const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" sticky="top">
       <Navbar.Brand>Trekk</Navbar.Brand>
       <Nav className="mr-auto">
         <LinkContainer to="/user">
