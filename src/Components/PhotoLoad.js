4ad9e29bb5b08725de5420b34092e2ddaecad1c3
Patricia de Boer <pdeboer05@gmail.com>
2019-07-03 02:02:14 -0500

    loading file and rendering modal

--- src/Components/PhotoLoad.js
+++ src/Components/PhotoLoad.js
@@ -3,34 +3,45 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import UserProfileHeader from './UserProfileHeader';
 import bucket from '../firebase';
+import { Modal, Button } from 'react-bootstrap';
+
 export const PhotoLoad = () => {
   const [progressValue, setProgressValue] = useState(0);
-
+  const [loaded, setLoaded] = useState(false);
+  function handleClose() {
+    setLoaded(false);
+  }
   function handleChange(evt) {
-    alert('Hi');
     const file = evt.target.files[0];
+
     //TODO change tripID in Photoloader
     const tripId = 'qbzVqq6smFB5avawpsRX';
-    // console.log(firebase.storage().ref())
 
     //Create a storage ref
     const storageRef = firebase
       .storage()
       .ref(`tripImages/${tripId}/${file.name}`);
-    // bucket.upload(`/tripImages/${tripId}/${file.name}`, function(err,file,apiResponse){
-    //   //DO SOmething
-    //   console.log('Success!')
-    // })
 
     // //Upload File
     let uploadFile = storageRef.put(file);
 
     //Update Progress Bar
-    uploadFile.on('state_changed',
-    function progress(snapshot){
-      const percentage = (snapshot.bytesTransferred /snapshot.totalBytes)*100;
-      setProgressValue(percentage);
-    })
+    uploadFile.on(
+      'state_changed',
+      function progress(snapshot) {
+        const percentage =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(percentage);
+        console.log(snapshot);
+        setProgressValue(percentage);
+      },
+      function error(err) {
+        console.log(err);
+      },
+      function complete() {
+        setLoaded(true);
+      }
+    );
   }
   return (
     <div>
@@ -40,6 +51,19 @@ export const PhotoLoad = () => {
       </progress>
       <br />
       <input type="file" id="fileButton" onChange={handleChange} />
+      {/* {loaded && (
+        <div><h1>SUCCESS!</h1></div>
+      )} */}
+      <Modal show={loaded} onHide={handleClose}>
+        <Modal.Body>
+          <h1 style={{textAlign:'center'}}>Loaded Successfully!</h1>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="info" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
