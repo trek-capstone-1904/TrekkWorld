e64b54b00ac0bbd0fd32f2fd8e8d2e2cd6da5ee5
Monica Barboza <barboza.monica2@gmail.com>
2019-07-09 10:34:15 -0500

    time ago formatting

--- src/Components/SingleTrip/Journal/AllNotesCard.js
+++ src/Components/SingleTrip/Journal/AllNotesCard.js
@@ -1,27 +1,21 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import timeago from "epoch-timeago";
 
-
-export const AllNotesCard = (props) => {
-  console.log(props.note.time.toDate().toDateString())
-
+export const AllNotesCard = props => {
   return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{props.note.userName}</Card.Title>
+        {props.note.time && (
+          <Card.Subtitle>
+            {timeago(props.note.time.seconds * 1000)}
+          </Card.Subtitle>
+        )}
+        <Card.Text>{props.note.note}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-      <Card>
-        <Card.Body>
-          <Card.Title>{props.note.userName}</Card.Title>
-          {props.note.time && (
-
-            <Card.Subtitle>{props.note.time.toDate().toDateString()}</Card.Subtitle>
-
-          )}
-          <Card.Text>{props.note.note}</Card.Text>
-        </Card.Body>
-
-      </Card>
-
-
-  )
-}
-
-export default AllNotesCard
+export default AllNotesCard;
