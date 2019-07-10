7028867d472f96c12dacc62557ab8e90fa6ef7ae
Monica Barboza <barboza.monica2@gmail.com>
2019-07-09 19:02:49 -0500

    rendering my trekks in row on user profile

--- src/Components/User/UserProfileTrips.js
+++ src/Components/User/UserProfileTrips.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TripListItem from './TripListItem';
+import { CardGroup } from 'react-bootstrap';
 
 export const UserProfileTrips = props => {
   let trips = Object.entries(props.trips).map(tripObj => ({
@@ -10,11 +11,14 @@ export const UserProfileTrips = props => {
   trips.sort((a, b) => (a.date > b.date ? 1 : -1));
 
   return (
-    <div style={{ maxWidth: '50rem' }}>
+    <div style={{ maxWidth: '100rem', justifyContent: "space-around" }}>
       <h1>My Trekks</h1>
+      <CardGroup>
       {trips.map(trip => (
         <TripListItem key={trip.id} tripId={trip.id} />
-      ))}
+        ))}
+        </CardGroup>
+
     </div>
   );
 };
