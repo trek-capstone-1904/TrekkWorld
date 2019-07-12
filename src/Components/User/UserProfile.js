63587255c7e4a6705d8afc4911aff0c2a51d1a9d
Monica Barboza <barboza.monica2@gmail.com>
2019-07-11 23:20:08 -0500

    icons and stats updated

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,21 +1,24 @@
-import React, { useContext } from 'react';
-import { useDocument } from 'react-firebase-hooks/firestore';
-import { Tabs, Tab, Media } from 'react-bootstrap';
-import db from '../../firebase';
-import { UserProfileHeader, UserProfileTrips, WorldMap } from '../index.js';
-import styles from '../UserProfile.module.css';
+import React, { useContext } from "react";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { Tabs, Tab, Media } from "react-bootstrap";
+import db from "../../firebase";
+import { UserProfileHeader, UserProfileTrips, WorldMap } from "../index.js";
+import styles from "../UserProfile.module.css";
 import {
   InputGroup,
   Button,
   FormControl,
   CardGroup,
   Spinner,
-  Card,
-} from 'react-bootstrap';
-import userContext from '../../Contexts/userContext';
-import TrekkList from '../SingleTrip/TrekkList';
-import BucketList from './BucketList'
-import {MdDateRange} from 'react-icons/md'
+  Card
+} from "react-bootstrap";
+import userContext from "../../Contexts/userContext";
+import TrekkList from "../SingleTrip/TrekkList";
+import BucketList from "./BucketList";
+import { MdDateRange } from "react-icons/md";
+import luggage from "./luggage.png";
+import globe from "./globe.png";
+import calendar from "./calendar.png";
 
 const daysUntil = trips => {
   let tripDates = Object.values(trips)
@@ -34,61 +37,170 @@ const daysUntil = trips => {
 };
 
 export const UserProfile = props => {
-
-  console.log('props.match.params',props.match.params)
+  console.log("props.match.params", props.match.params);
   const loggedInUser = useContext(userContext);
   let user;
-  if(props.match.params.userId){
-    user=props.match.params.userId
-  }else{
-    user=loggedInUser.uid
+  if (props.match.params.userId) {
+    user = props.match.params.userId;
+  } else {
+    user = loggedInUser.uid;
   }
-  const [value, loading, error] = useDocument(
-    db.doc(`Users/${user}`),
-    {
-      valueListenOptions: { includeMetadataChanges: true },
-    }
-  );
+  const [value, loading, error] = useDocument(db.doc(`Users/${user}`), {
+    valueListenOptions: { includeMetadataChanges: true }
+  });
   if (error) throw error;
   if (loading) return <Spinner animation="grow" variant="info" />;
   if (value) {
     const userInfo = value.data();
-    console.log("userInfo", userInfo)
+    console.log("userInfo", userInfo);
     return (
       <div>
         <UserProfileHeader user={userInfo} />
         <div className={styles.userBody}>
-          <div style={{ minWidth: '60vw' }} className={styles.userProfileBody}>
-            {userInfo.bucketList && <BucketList trips={'123'} />}
+          <div style={{ minWidth: "60vw" }} className={styles.userProfileBody}>
+            {userInfo.bucketList && <BucketList trips={"123"} />}
           </div>
-        <h1>My Bucket List</h1>
-          <div style={{ minWidth: '60vw' }} className={`${styles.userProfileBody} ${styles.userProfileBackground}`}>
+          <h1>My Bucket List</h1>
+          <div
+            style={{ minWidth: "60vw" }}
+            className={`${styles.userProfileBody} ${
+              styles.userProfileBackground
+            }`}
+          >
             {userInfo.Trips && <UserProfileTrips trips={userInfo.Trips} />}
           </div>
           <div className={styles.userStats}>
-
-              {/* <Card.Header>Trekkers Stats</Card.Header> */}
-              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: "space-around"}}>
-              <Media>
-              {/* <MdDateRange style={{minHeight: "10rem"}} /> */}
-              <img alt="img"src='src/Components/User/iconfinder_ic_card_travel_48px_3669137.png' />
-                <Card>
-                  {/* <Card.Header>
-                    <MdDateRange  />Days Until Next Trekk</Card.Header> */}
+            {/* <Card.Header>Trekkers Stats</Card.Header> */}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around"
+              }}
+            >
+              {userInfo.Trips && (
+                <Media>
+                  <img
+                    className="align-self-center mr-3"
+                    alt="img"
+                    src={calendar}
+                    style={{ opacity: ".4", maxHeight: "5rem" }}
+                  />
+                  <Media.Body
+                    style={{ minHeight: "5rem", justfContent: "center" }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray",
+                        fontWeight: "bold",
+                        margin: "0rem"
+                      }}
+                    >
+                      {daysUntil(userInfo.Trips)}
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray"
+                      }}
+                    >
+                      {" "}
+                      days until the next trekk
+                    </p>
+                  </Media.Body>
+                </Media>
+              )}
+              {/* <Card>
+                  <Card.Header>
+                    <MdDateRange  />Days Until Next Trekk</Card.Header>
                   {userInfo.Trips && (
                     <Card.Title>{daysUntil(userInfo.Trips)} Days Until The Next Trekk</Card.Title>
                   )}
-                </Card>
-              </Media>
-                <Card>
+                </Card> */}
+              {userInfo.Trips && (
+                <Media>
+                  <img
+                    className="align-self-center mr-3"
+                    alt="img"
+                    src={luggage}
+                    style={{ opacity: ".4", maxHeight: "5rem" }}
+                  />
+                  <Media.Body
+                    style={{ minHeight: "5rem", justfContent: "center" }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray",
+                        fontWeight: "bold",
+                        margin: "0rem"
+                      }}
+                    >
+                      {Object.values(Object.values(userInfo.Trips)).length}
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray"
+                      }}
+                    >
+                      trekks completed
+                    </p>
+                  </Media.Body>
+                </Media>
+              )}
+
+              {/* <Card>
                   <Card.Header>Number Of Trekks</Card.Header>
                   {userInfo.Trips && (
                     <Card.Title>
                       {Object.values(Object.values(userInfo.Trips)).length}
                     </Card.Title>
                   )}
-                </Card>
-                <Card >
+                </Card> */}
+
+              {userInfo.countriesVisited && (
+                <Media>
+                  <img
+                    className="align-self-center mr-3"
+                    alt="img"
+                    src={globe}
+                    style={{ opacity: ".4", maxHeight: "5rem" }}
+                  />
+                  <Media.Body
+                    style={{ minHeight: "5rem", justfContent: "center" }}
+                  >
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray",
+                        fontWeight: "bold",
+                        margin: "0rem"
+                      }}
+                    >
+                      {Object.values(Object.values(userInfo.Trips)).length}%
+                    </p>
+                    <p
+                      style={{
+                        fontSize: "1.5rem",
+                        alignContent: "center",
+                        color: "gray"
+                      }}
+                    >
+                      {" "}
+                      of countries visited
+                    </p>
+                  </Media.Body>
+                </Media>
+              )}
+
+              {/* <Card >
                   <Card.Header>% of Countries Visited</Card.Header>
                   {userInfo.countriesVisited && (
                     <Card.Title>
@@ -98,13 +210,13 @@ export const UserProfile = props => {
                       %
                     </Card.Title>
                   )}
-                </Card>
-              </div>
+                </Card> */}
+            </div>
 
             {userInfo.countriesVisited && (
-              <Card style={{ margin: '.5rem', minWidth: "50%" }}>
+              <Card style={{ margin: ".5rem", minWidth: "50%" }}>
                 <Card.Header>
-                  Countries Visited ({userInfo.countriesVisited.length})
+                {userInfo.countriesVisited.length} Countries Visited
                 </Card.Header>
                 <WorldMap />
               </Card>
