5bb827a2acd5251775c240b028d2b243b18f37a6
Patricia de Boer <pdeboer05@gmail.com>
2019-07-13 15:39:37 -0500

    bucket list wrap

--- src/Components/TripPlanning/Toast.js
+++ src/Components/TripPlanning/Toast.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useDocument, useCollection } from 'react-firebase-hooks/firestore';
 
-export const Toast = () => {
-  return (
-    <div></div>
-  )
-}
+export const Toast = (props) => {
+  const {tripId} = props
+  const [trekk, loading, error] = useCollection(
+    db
+      .collection('Trips')
+      .doc(tripId)
+      .collection('TrekkList'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+  return <div />;
+};
 
-export default Toast
+export default Toast;
